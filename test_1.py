8e50f5975e4a6f242073590814ca75cf7ec26aab
shan458 <77813639+shan458@users.noreply.github.com>
2021-04-15 14:57:07 -0700

    finished pseudo code

--- test_1.py
+++ test_1.py
@@ -9,3 +9,29 @@ If amounts are equal for both options, we output to the user "Option 1 and Optio
 If option 1 is better, then we output to the user "Option 1 is better"
 If option 2 is better, then we output to the user "Option 2 is better"
 """
+
+"""
+# Option 1
+    return 100 * 10 
+
+# Option 2
+    amount = 1
+    list1 = []
+    loop 10 times
+        add amount to list1
+        amount *= 2
+    sum = sum of all items in loop
+    return sum
+# main
+    var1 = option1
+    var2 = option2
+
+    if var1 = var2
+        "Option 1 and Option 2 pays the same"
+    if var1 < var2
+        "Option 2 is better"
+    else
+        "Option 1 is better"
+
+main
+"""
